2fb21997929e84e866edd0c19ceb5e7b4049ac97
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-15 20:17:41 +0330

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result += calc(i);
+            }
+            Console.WriteLine("the sum is:" + result);
+            Console.ReadKey();
+        }
+
+        static double calc(int n)
+        {
+            if(n==1)
+            {
+                return 0.5;
+            }
+            else
+            {
+                return (double)n / (Math.Pow(n, 2) + calc(n - 1));
+            }
+        }
+    }
+}
